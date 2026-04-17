7e4fb575b9e195ae5cf05bd8d0dd587b8913f4da
zaraknyu <razenxizu@gmail.com>
2026-04-17 11:51:39 +0000

    mumba: Initial skeleton tree

---
+++ device.mk
@@ -0,0 +1,18 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := false
+
+# Shipping API
+BOARD_SHIPPING_API_LEVEL := 202404
+PRODUCT_SHIPPING_API_LEVEL := 36
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/motorola
+
+# Inherit from the proprietary files makefile.
+$(call inherit-product, vendor/motorola/mumba/mumba-vendor.mk)
