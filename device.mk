d332ede690235af15edb5915032a56f57a4e2fdb
zaraknyu <razenxizu@gmail.com>
2026-07-02 06:31:21 +0000

    mumba: Build MotoEuicc

--- device.mk
+++ device.mk
@@ -128,6 +128,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# eUICC
+PRODUCT_PACKAGES += \
+    MotoEuicc
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
