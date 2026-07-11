59c55cf0eb806fbca17ae4f10317de10ed12b4be
zaraknyu <razenxizu@gmail.com>
2026-07-11 19:43:32 +0000

    mumba:  Build ipacm

--- device.mk
+++ device.mk
@@ -181,6 +181,12 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    IPACM_Filter_cfg.xml
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
