2da15be28cda433059342cdf8a1868a448ce5f03
zaraknyu <razenxizu@gmail.com>
2026-07-11 19:11:39 +0000

    mumba:  Setup A/B

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,23 @@
 
 DEVICE_PATH := device/motorola/mumba
 
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    init_boot \
+    product \
+    pvmfw \
+    recovery \
+    system \
+    system_dlkm \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot \
+    vendor_dlkm
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
