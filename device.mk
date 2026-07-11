824a95a826c090f2a5d1dc23586f559a3be55de1
zaraknyu <razenxizu@gmail.com>
2026-07-11 19:01:53 +0000

    mumba: Add partition flags

--- device.mk
+++ device.mk
@@ -8,6 +8,18 @@ PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := false
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
+# Mountpoints/Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_PVMFW_IMAGE := true
+
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_fsg_mountpoint \
+    vendor_super_fsg_mountpoint \
+    vendor_super_modem_mountpoint
+
 # Shipping API
 BOARD_SHIPPING_API_LEVEL := 202404
 PRODUCT_SHIPPING_API_LEVEL := 36
